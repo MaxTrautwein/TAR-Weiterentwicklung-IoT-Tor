3550aec31e4238b4664d87ee59d9d0a912a1fb34
MaxTrautwein <trautwein.max@googlemail.com>
2021-09-04 09:23:51 +0000

    updated and code

--- Nodes/Logical/and.js
+++ Nodes/Logical/and.js
@@ -3,13 +3,29 @@ module.exports = function(RED) {
     
     function AndNode(config) {
         RED.nodes.createNode(this,config);
-
-        this.I1 = config.I1;
+        
+        //TODO get size from inputs property
+        //Array().fill may not work with IE
+        config.data = Array(4).fill(undefined);
+        
         
 
         var node = this;
         node.on('input', function(msg) {
-           
+            //cache Recived Value
+            config.data[msg.__port] = msg.payload;
+
+            //And Logic
+            var result = true;
+            config.data.forEach(element => {
+                if (element == false){
+                    result = false; 
+                }
+            });
+
+            var msg = { payload:result }
+            node.send(msg);
+
         });
     }
     //Register Node
